9d064083d64788bc5c4b6a2a027f0d0d45e1a95b
Zach <zachariedupain@hotmail.fr>
2023-07-01 11:41:49 +0400

    fix: add safeguard in [country]

--- components/show-country/country-details-displayer.tsx
+++ components/show-country/country-details-displayer.tsx
@@ -33,62 +33,117 @@ const CountryDetailsDisplayer: FC<CountryDetailsDisplayerProps> = ({
 
     return (
         <div className="w-1/2 text-lg grid grid-cols-2 gap-4" id="detailCountryContainer">
-            <div>
-                <p>
-                    <span>Capital: </span> {capital}
-                </p>
-                <p>
-                    <span>Area: </span> {area}
-                </p>
-                <p>
-                    <span>Currency: </span>
-                    {Object.values(currencies)[0].name}
-                </p>
+            <div id="capital-area-currency">
+                {capital ? (
+                    <p>
+                        <span>Capital: </span> {capital}
+                    </p>
+                ) : (
+                    ''
+                )}
+
+                {area ? (
+                    <p>
+                        <span>Area: </span> {area}
+                    </p>
+                ) : (
+                    ''
+                )}
+
+                {currencies ? (
+                    <p>
+                        <span>Currency: </span>
+                        {Object.values(currencies)[0].name}
+                    </p>
+                ) : (
+                    ''
+                )}
             </div>
 
-            <div>
-                <p>
-                    <span>Population: </span>
-                    {population}
-                </p>
-                <p>
-                    <span>Demonym: </span>
-                    {demonyms.eng.m}
-                </p>
-                <p>
-                    <span>Language: </span> {Object.values(languages)[0]}
-                </p>
+            <div id="population-languages">
+                {population ? (
+                    <p>
+                        <span>Population: </span>
+                        {population}
+                    </p>
+                ) : (
+                    ''
+                )}
+
+                {demonyms ? (
+                    <p>
+                        <span>Demonym: </span>
+                        {demonyms.eng.m}
+                    </p>
+                ) : (
+                    ''
+                )}
+
+                {languages ? (
+                    <p>
+                        <span>Language: </span> {Object.values(languages)[0]}
+                    </p>
+                ) : (
+                    ''
+                )}
             </div>
 
-            <div>
-                <p>
-                    <span>Independency: </span>
-                    {independent ? 'Yes' : 'No'}
-                </p>
-                <p>
-                    <span>Member of United Nations: </span>
-                    {unMember ? 'Yes' : 'No'}
-                </p>
+            <div id="independent-unMember">
+                {independent ? (
+                    <p>
+                        <span>Independency: </span>
+                        {independent ? 'Yes' : 'No'}
+                    </p>
+                ) : (
+                    ''
+                )}
+
+                {unMember ? (
+                    <p>
+                        <span>Member of United Nations: </span>
+                        {unMember ? 'Yes' : 'No'}
+                    </p>
+                ) : (
+                    ''
+                )}
             </div>
 
-            <div>
-                <p>
-                    <span>Region: </span>
-                    {region}
-                </p>
-                <p>
-                    <span>Subregion: </span>
-                    {subregion}
-                </p>
+            <div id="regions-timezones-startWeek">
+                {region ? (
+                    <p>
+                        <span>Region: </span>
+                        {region}
+                    </p>
+                ) : (
+                    ''
+                )}
 
-                <p>
-                    <span>Timezone: </span>
-                    {timezones[0]}
-                </p>
-                <p>
-                    <span>Start week: </span>
-                    {startOfWeek}
-                </p>
+                {subregion ? (
+                    <p>
+                        <span>Subregion: </span>
+                        {subregion}
+                    </p>
+                ) : (
+                    ''
+                )}
+
+                {timezones ? (
+                    <p>
+                        <span>Timezone: </span>
+                        {timezones[0]}
+                    </p>
+                ) : (
+                    ''
+                )}
+
+                {startOfWeek ? (
+                    <p>
+                        <span>Start week: </span>
+                        {startOfWeek}
+                    </p>
+                ) : (
+                    ''
+                )}
             </div>
         </div>
     )
